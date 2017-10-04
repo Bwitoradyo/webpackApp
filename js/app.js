877d66b30d596b825598f12be6ab3a5b1a106527
bwitoradyo <bambang.witoradyo@gmail.com>
2017-10-03 22:29:14 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+require("./login");
+
+document.write("Welcome to the very Gigantic Sumologic hair concerts Baby Now!");
+
+console.log("App loader");
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+
+module.exports = {
+	
+  context: path.resolve("js"),	
+  entry:["./utils.js", "./app.js"],
+  output: {
+    path: path.resolve("build/js/"),
+    publicPath: "/public/assets/js/",
+    filename:"bundle.js"
+  },
+  
+  devServer: {
+    contentBase: "public"
+  },
+
+  watch: true,
+
+  module: {	  
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "jshint-loader",
+        enforce: "pre"
+      },
+     
+      {
+        test:/\.es6$/,
+	exclude: /node_modules/, 
+	loader: "babel-loader"
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [ ".js", ".es6"]
+  }
+}
